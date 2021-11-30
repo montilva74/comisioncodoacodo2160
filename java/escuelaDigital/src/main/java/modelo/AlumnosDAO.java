0638dcaf06b4365e6e6557b228e373e67c89aacc
alba montilva <montilva74@gmail.com>
2021-11-30 13:29:43 -0500

    Creamos métodos para la clase AlumnosDAO

--- java/escuelaDigital/src/main/java/modelo/AlumnosDAO.java
+++ java/escuelaDigital/src/main/java/modelo/AlumnosDAO.java
@@ -1,13 +1,116 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modelo;
 
-/**
- *
- * @author ALBA
- */
+import config.Conexion;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class AlumnosDAO {
-    
+    Connection conexion;
+    public AlumnosDAO(){
+        Conexion con = new Conexion();
+        conexion = con.getConection();
+    }
+    public List<Alumnos> listarAlumnos(){
+       PreparedStatement ps;
+       ResultSet rs;
+       List<Alumnos> lista = new ArrayList<>();
+       /*List es abstracta*/
+       try{
+           ps = conexion.prepareStatement("SELECT * FROM participantes");
+           rs = ps.executeQuery();
+           
+           while(rs.next()) {
+               int id = rs.getInt("id");
+               String nombre = rs.getString("nombre");
+               String apellido = rs.getString("apellido");
+               String email = rs.getString("email");      
+               int telefono = rs.getInt("telefono");  
+               Alumnos alumnos = new Alumnos(id, nombre, apellido, email, telefono);
+               lista.add(alumnos);
+           }
+           return lista;
+           
+       }catch(SQLException e){
+           System.out.println(e.toString());
+           return null;
+       }
+    }
+    public Alumnos mostrarAlumno(int _id) {
+        PreparedStatement ps;
+        ResultSet rs;
+        Alumnos alumno = null;
+        
+        try{
+           ps = conexion.prepareStatement("SELECT * FROM" + "participantes WHERE id=?");
+           ps.setInt(1, _id);
+           rs = ps.executeQuery();
+           
+           while(rs.next()) {
+               int id = rs.getInt("id");
+               String nombre = rs.getString("nombre");
+               String apellido = rs.getString("apellido");
+               String email = rs.getString("email");      
+               int telefono = rs.getInt("telefono");  
+               alumno = new Alumnos(id, nombre, apellido, email, telefono);
+           }
+           return alumno;
+           
+        }catch(SQLException e){
+            System.out.println(e.toString());
+            return null;
+        }
+    }
+    public boolean insertarAlumno(Alumnos alumno){
+        PreparedStatement ps;
+        try{
+        ps = conexion.prepareStatement("INSERT INTO participantes" 
+                + "(nombre,apellido,email,telefono)" 
+                + "VALUES (?,?,?,?)");
+        ps.setString(1, alumno.getNombre());
+        ps.setString(2, alumno.getApellido());
+        ps.setString(3, alumno.getEmail());
+        ps.setInt(4, alumno.getTelefono());
+        ps.execute();
+        return true;
+        }catch(SQLException e){
+            System.out.println(e.toString());
+            return false;
+        }
+    }
+    public boolean actualizarAlumno(Alumnos alumno) {
+       PreparedStatement ps;
+        try{
+        ps = conexion.prepareStatement("UPDATE "
+        + "participantes SET nombre=?"
+        + "participantes SET apellido=?"
+        + "participantes SET email=?"
+        + "participantes SET telefono=? WHERE  id=?");
+        ps.setString(1, alumno.getNombre());
+        ps.setString(2, alumno.getApellido());
+        ps.setString(3, alumno.getEmail());
+        ps.setInt(4, alumno.getTelefono());
+        ps.setInt(5, alumno.getId());
+        ps.execute();
+        return true;
+        }catch(SQLException e){
+            System.out.println(e.toString());
+            return false;
+        } 
+    }
+    public boolean eliminarAlumno(int _id) {
+     PreparedStatement ps;
+          try{
+            ps = conexion.prepareStatement("DELETE FROM "
+                    + "participantes WHERE id=?");
+            ps.setInt(1,_id);
+            ps.execute();
+            return true;
+          }catch(SQLException e){
+              System.out.println(e.toString());
+            return false;
+          }
+    }
 }
--- java/escuela_Digital/src/docente/Docente.java
+++ java/escuela_Digital/src/docente/Docente.java
@@ -4,11 +4,12 @@ package docente;
 public class Docente {
     public String nombre,apellidos;
     public int edad;
-}
+
 
 public Docente(String nombre,String apellidos,int edad) {
     this.nombre=nombre;
     this.apellidos=apellidos;
     this.edad=edad;
+}
 }
 
